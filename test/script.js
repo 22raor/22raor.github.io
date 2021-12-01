d6060ef163cf002d5c197b1b83a7b4d74f3d7733
Rishi Rao <22raor@gmail.com>
2021-12-01 12:21:25 -0500

    Add test controllers

---
+++ test/script.js
@@ -0,0 +1,34 @@
+var currentUser = "";
+if(document.cookie!=""){
+  
+  let temp = document.cookie.replace("user=", "");
+  document.getElementById("logger").innerText = "You are logged in";
+  currentUser = temp;
+}
+
+function login() {
+  var m = document.getElementById("logbox").value;
+ document.cookie = "user=" + m;
+ currentUser = m; document.getElementById("logger").innerText = "You are logged in";
+}
+
+
+  var config = {
+    apiKey: "AIzaSyCgDMqXYx3JLTgcY6Nc9xv6Rt2EOuU4cuk",
+    authDomain: "shortener-97531.firebaseapp.com",
+    databaseURL: "https://shortener-97531-default-rtdb.firebaseio.com/",
+    projectId: "shortener-97531",
+    storageBucket: "shortener-97531.appspot.com",
+    messagingSenderId: "117527348315",
+  };
+  firebase.initializeApp(config);
+var database = firebase.database();
+
+function control(m){
+  
+     firebase.database().ref(currentUser).set(m);
+  
+
+  
+  
+}
